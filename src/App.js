538e94ee5b582abb575612b16775c855aeb741df
lifeoncode <nduduzo@yellogarden.co.za>
2023-02-03 08:22:25 +0200

    moved components, path changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Header from "./header/Header";
-import Home from "./pages/home/Home";
+import Header from "./components/header/Header";
+// import Home from "./pages/home/Home";
+// import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
 
 function App() {
   return (
     <>
       <Header />
-      <Home />
+      <Write />
     </>
   );
 }
