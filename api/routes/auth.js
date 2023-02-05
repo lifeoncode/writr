65684f9076bf1f966568665e297e94127b4cc07e
lifeoncode <nduduzo@yellogarden.co.za>
2023-02-05 19:50:04 +0200

    routes and user authentication

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,25 +2,38 @@ const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
-// Sign up
+//REGISTER
 router.post("/register", async (req, res) => {
   try {
-    // grab data from request body
-    const { username, email, password } = req.body;
-    // hash the password
-    // const salt = bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, 10);
-    // construct user
-    const newUser = new User({ username, email, password: hashedPassword });
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(req.body.password, salt);
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPass,
+    });
+
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (err) {
-    console.log(err);
     res.status(500).json(err);
   }
 });
 
-// Login
+//LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(400).json("Wrong credentials!");
 
-// export
+    const validated = await bcrypt.compare(req.body.password, user.password);
+    !validated && res.status(400).json("Wrong credentials!");
+
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
