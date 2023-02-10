55e47ab094df9514773438b0f84dd6e86b6661c1
lifeoncode <nduduzo@yellogarden.co.za>
2023-02-10 13:09:31 +0200

    file restructure

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/header/Header";
-import Home from "./pages/home/Home";
-import Register from "./pages/register/Register";
-import Login from "./pages/login/Login";
-import Write from "./pages/write/Write";
-import Settings from "./pages/settings/Settings";
-import Single from "./pages/single/Single";
-import { useContext } from "react";
-import { Context } from "./context/Context";
-
 function App() {
-  const { user } = useContext(Context);
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={user ? <Home /> : <Register />} />
-        <Route path="/login" element={user ? <Home /> : <Login />} />
-        <Route path="/write" element={user ? <Write /> : <Login />} />
-        <Route path="/settings" element={user ? <Settings /> : <Login />} />
-        <Route path="/post/:id" element={<Single />} />
-      </Routes>
-    </Router>
+    <main>
+      <header>
+        <div className="container">
+          <span className="logo">writr.</span>
+          <nav>
+            <a href="/login">Login</a>
+            <a href="/register">Sign up</a>
+          </nav>
+        </div>
+      </header>
+    </main>
   );
 }
 
